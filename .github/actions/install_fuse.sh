47a6b0b8a5e4361ad3ee861730d7678de587641b
th-os <thomas.oswald@stud.uni-r.de>
2019-10-09 11:53:06 +0200

    another test

--- .github/actions/install_fuse.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get install ninja-build
-wget https://github.com/libfuse/libfuse/releases/download/fuse-$1/fuse-$1.tar.xz
-pip install meson
-tar xf fuse-$1.tar.xz
-cd fuse-$1; mkdir build; cd build; meson ..; ninja; sudo ninja install
-ls /usr/local/lib
-ls /lib
-sudo ln -s /usr/local/lib/x86_64-linux-gnu/libfuse3.so.$1 /lib/x86_64-linux-gnu/libfuse3.so.3
