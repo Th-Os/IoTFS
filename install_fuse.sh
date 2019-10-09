c5bcd420250111c3c45158fdb7cd3b68b47dff77
th-os <thomas.oswald@stud.uni-r.de>
2019-10-09 14:39:09 +0200

    another test

--- install_fuse.sh
+++ install_fuse.sh
@@ -3,6 +3,4 @@ wget https://github.com/libfuse/libfuse/releases/download/fuse-$1/fuse-$1.tar.xz
 pip3 install meson
 tar xf fuse-$1.tar.xz
 cd fuse-$1; mkdir build; cd build; meson ..; ninja; sudo ninja install
-ls /usr/local/lib
-ls /lib
 sudo ln -s /usr/local/lib/x86_64-linux-gnu/libfuse3.so.$1 /lib/x86_64-linux-gnu/libfuse3.so.3
